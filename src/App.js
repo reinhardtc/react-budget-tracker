5b6b67b004fd601774decbc4fd779c9400a024b4
colin reinhardt <reinhardtc22@gmail.com>
2022-01-24 08:10:52 -0600

    added new budget modal

--- src/App.js
+++ src/App.js
@@ -1,27 +1,31 @@
 import { Stack, Button } from 'react-bootstrap';
 import Container from 'react-bootstrap/Container';
+import AddBudgetModal from './Components/AddBudgetModal';
 import BudgetCard from './Components/BudgetCard';
 
 function App() {
 	return (
-		<Container>
-			<Stack direction="horizontal" gap="2" className="mb-4">
-				<h1 className="me-auto">Budgets</h1>
-				<Button variant="primary">Add Budget</Button>
-				<Button variant="outline-primary">Add Expense</Button>
-			</Stack>
-			<div
-				className="budgetCard"
-				style={{
-					display: 'grid',
-					gridTemplateColumns: 'repeat(auto-fill, minmax(300px, 1fr))',
-					gap: '1rem',
-					alignItems: 'flex-start',
-				}}
-			>
-				<BudgetCard name="Food" amount={1000} max={1000}></BudgetCard>
-			</div>
-		</Container>
+		<>
+			<Container>
+				<Stack direction="horizontal" gap="2" className="mb-4">
+					<h1 className="me-auto">Budgets</h1>
+					<Button variant="primary">Add Budget</Button>
+					<Button variant="outline-primary">Add Expense</Button>
+				</Stack>
+				<div
+					className="budgetCard"
+					style={{
+						display: 'grid',
+						gridTemplateColumns: 'repeat(auto-fill, minmax(300px, 1fr))',
+						gap: '1rem',
+						alignItems: 'flex-start',
+					}}
+				>
+					<BudgetCard name="Food" amount={1000} max={1000}></BudgetCard>
+				</div>
+			</Container>
+			<AddBudgetModal show />
+		</>
 	);
 }
 
---
+++ src/Components/AddBudgetModal.js
@@ -0,0 +1,30 @@
+import { Form, Modal, Button } from 'react-bootstrap';
+
+export default function AddBudgetModal({ show, showHandleClose }) {
+	function handleSubmit(e) {}
+
+	return (
+		<Modal show={show} onHide={showHandleClose}>
+			<form onSubmit={handleSubmit}>
+				<Modal.Header closeButton>
+					<Modal.Title>New Budget</Modal.Title>
+				</Modal.Header>
+				<Modal.Body>
+					<Form.Group className="mb-3" controlId="name">
+						<Form.Label>Name</Form.Label>
+						<Form.Control type="text"></Form.Control>
+					</Form.Group>
+					<Form.Group className="mb-3" controlId="max">
+						<Form.Label>Maximum Spending</Form.Label>
+						<Form.Control type="number" required min={0} step={0.01}></Form.Control>
+					</Form.Group>
+					<div className="d-flex justify-content-end">
+						<Button variant="primary" type="submit">
+							Add
+						</Button>
+					</div>
+				</Modal.Body>
+			</form>
+		</Modal>
+	);
+}
